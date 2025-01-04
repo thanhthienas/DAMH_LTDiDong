e4a37754ac5695e3ac1eb2efbdc3a89e2d87c537
NewTranWork <143317056+NewTranWork@users.noreply.github.com>
2025-01-04 19:06:45 +0700

    Chỉnh sửa thêm đăng xuất cho app
    Mình chỉnh sửa ở Main Screen để có chức năng đăng xuất và ra giao diện danh sách các chức năng như Admin( Lưu ý: Mình có chỉnh sửa ở hàm Main, .env và AndroidManifest vì máy mình cần phải chỉnh sửa để chạy được, TUYỆT ĐỐI KHÔNG XÓA VÀ CHỈNH SỬA)

--- nhom6_android_studio/lib/main.dart
+++ nhom6_android_studio/lib/main.dart
@@ -1,11 +1,25 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:nhom6_android_studio/screens/login_screen.dart';
 
 Future<void> main() async {
   await dotenv.load(fileName: ".env");
+  // Dòng code riêng của mình để chạy riêng trên mày(không được sửa) (Đức)
+  // HttpOverrides.global = MyHttpOverrides();
   runApp(const MyApp());
 }
+
+// Dòng code riêng của mình để chạy riêng trên mày(không được sửa) (Đức)
+// class MyHttpOverrides extends HttpOverrides {
+//   @override
+//   HttpClient createHttpClient(SecurityContext? context) {
+//     return super.createHttpClient(context)
+//       ..badCertificateCallback = (X509Certificate cert, String host, int port) => true;
+//   }
+// }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
